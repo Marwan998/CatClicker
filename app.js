c241c0847087db5a6ce171ee979abcc16c34188f
Marwan998 <m.alramahi98@gmail.com>
2019-09-17 12:11:36 +0300

    Requirments change 1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var elm = document.querySelector('img');
-var inc = document.querySelector('p');
-elm.addEventListener('click',function () {
-  inc.innerHTML = parseInt(inc.innerText)+1;
-});
+var elm = document.querySelectorAll('img');
+var inc = document.querySelector('#inc');
+elm.forEach(function(img){
+  img.addEventListener('click',function () {
+    inc.innerHTML = parseInt(inc.innerText)+1;
+    })
+  }
+);
+var cat1 = "lolo";
+var cat2 = "katy";
+document.querySelector('#cat1').innerText=cat1;
+document.querySelector('#cat2').innerText=cat2;
